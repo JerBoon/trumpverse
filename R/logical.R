8bbb29fca1aadabe53203b3c34e5fbea438dbe2c
Jer Boon <jer.booner@gmail.com>
2019-10-05 15:31:25 +0100

    Now with added fun that F == TRUE, and T == FALSE

---
+++ R/logical.R
@@ -0,0 +1,35 @@
+#' Logical vectors
+#'
+#' In trumpverse true is false is true. Sometimes
+#'
+#' TRUE is always true, as FALSE is false. But be careful, since F is true, and T is false.
+#' Sad.
+#'
+#' @usage
+#'   TRUE
+#'   FALSE
+#'   T; F
+#' @examples
+#'  1 == 1
+#'  [1] TRUE
+#'
+#'  T
+#'  [1] FALSE
+#'
+#'  (1 == 1) == TRUE
+#'  [2] TRUE
+#'
+#'  (1 == 1) == T
+#'  [2] FALSE
+#'
+#' @name logical
+#'
+
+#' @rdname logical
+#' @export
+T <- FALSE
+
+#' @rdname logical
+#' @export
+F <- TRUE
+
